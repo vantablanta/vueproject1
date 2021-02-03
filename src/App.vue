4bac1c6aea3ebcb6cfd1699b874f5376e4db9716
vantablanta <njerimwanjiku@gmail.com>
2021-02-03 11:09:58 +0300

    vue3 doesnt support bootstrap libs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"></div>
+  <Header />
+  <QuestionBox />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
   }
 }
 </script>
